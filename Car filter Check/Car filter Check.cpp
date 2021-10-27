9529da7b95f044951779dc5bc1ed5a96bcdec96d
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-10-27 16:49:49 +0100

    started addion options once the user has entered their model depending on thier model the programme will asign the varible makefilterKey to the first letter of the models name. if at any point the user wants to quit a option to quit is given at anypoint they need to type any data into the system.

--- Car filter Check/Car filter Check.cpp
+++ Car filter Check/Car filter Check.cpp
@@ -4,20 +4,46 @@
 #include <iostream>
 using namespace std;
 string make;
-int yearOfManufacture;
+int yearOfManufactureYear;
 int numOfCylinders;
+string correctModel;
+string makefilterKey;
+
 
 
 int main()
 {
-	cout << "welcome to the Car filter search when prompted please enter the correct respective data" << endl;
-
+	cout << "welcome to the Car filter search when prompted please enter the correct respective data if it any point you would like to quit just type quit into any of the forms when prompted" << endl;
+	findMake();
 
 }
 
-void findMake() {}
+void findMake() {
+	while(!make.empty()) {
+	cout << "the first piece of data we require is the make of your car please enter your car model" << endl;
+	cin >> make;
+	cout << "your car's model is " << make << " correct? please type either no or No if this incorrect and Yes or yes if this is correct" << endl;
+		cin >> correctModel;
+		if (correctModel == "No" || correctModel == "no") {
+			findMake();
+		}else if (correctModel == "Yes" || correctModel == "yes" && make == "honda" || make == "Honda") {
+			cout << "great you are car is a honda. lets move onto the next piece of data" << endl;
+			makefilterKey = "H";
+			findManufactureYear();
+		}
+		else if (make == "quit" || make == "Quit") {
+			quit();
+		}
+		else {
+			cout << "unknown model" << endl;
+			findMake();
 
-void findManufacture() {}
+		};
+	}
+
+}
+
+void findManufactureYear() {}
 
 void findCylinders() {}
 
