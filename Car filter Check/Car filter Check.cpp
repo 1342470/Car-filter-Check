5652277f7a89d8572a5f18d0b4819713127d5bda
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-10-27 22:44:13 +0100

    added the exit function as well as inputting the year of manufacture.

--- Car filter Check/Car filter Check.cpp
+++ Car filter Check/Car filter Check.cpp
@@ -1,14 +1,17 @@
 // Car filter Check.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
+#include <stdlib.h>
 #include <iostream>
 using namespace std;
 string make;
-int yearOfManufactureYear;
+int yearOfManufacture;
 int numOfCylinders;
 string correctModel;
+string correctYear;
 string makefilterKey;
+string manfactureFilterKey;
 
+void exit(int status);
 
 
 int main()
@@ -19,7 +22,7 @@ int main()
 }
 
 void findMake() {
-	while(!make.empty()) {
+	while(make.empty()) {
 	cout << "the first piece of data we require is the make of your car please enter your car model" << endl;
 	cin >> make;
 	cout << "your car's model is " << make << " correct? please type either no or No if this incorrect and Yes or yes if this is correct" << endl;
@@ -28,24 +31,24 @@ void findMake() {
 			make = "";
 			findMake();
 		}else if (correctModel == "Yes" || correctModel == "yes" && make == "honda" || make == "Honda") {
-			cout << "great your  car is a honda. lets move onto the next piece of data" << endl;
+			cout << "great your car is a honda. lets move onto the next piece of data" << endl;
 			makefilterKey = "H";
 			findManufactureYear();
 		}else if (correctModel == "Yes" || correctModel == "yes" && make == "nisan" || make == "Nisan") {
-			cout << "great your  car is a nisan. lets move onto the next piece of data" << endl;
+			cout << "great your car is a nisan. lets move onto the next piece of data" << endl;
 			makefilterKey = "N";
 			findManufactureYear();
 		}else if (correctModel == "Yes" || correctModel == "yes" && make == "volov" || make == "Volov") {
-			cout << "great your  car is a volvo. lets move onto the next piece of data" << endl;
+			cout << "great your car is a volvo. lets move onto the next piece of data" << endl;
 			makefilterKey = "V";
 			findManufactureYear();
 		}else if (correctModel == "Yes" || correctModel == "yes" && make == "jaguar" || make == "Jaguar") {
-			cout << "great your  car is a jaguar. lets move onto the next piece of data" << endl;
+			cout << "great your car is a jaguar. lets move onto the next piece of data" << endl;
 			makefilterKey = "J";
 			findManufactureYear();
 		}
 		else if (make == "quit" || make == "Quit") {
-			quit();
+			exit(0);
 		}
 		else {
 			cout << "unknown model" << endl;
@@ -57,10 +60,31 @@ void findMake() {
 
 }
 
-void findManufactureYear() {
-	cout << "Next ";
+void findCylinders() {
 }
 
-void findCylinders() {}
+void findManufactureYear() {
+	while (!yearOfManufacture > 1995 && !yearOfManufacture < 2015) {
+		cout << "Next please enter your cars year of manufacture " << endl;
+		cin >> yearOfManufacture;
+		cout << "so your cars year of manufacture is " << yearOfManufacture << " ?  please type either no or No if this incorrect and Yes or yes if this is correct." << endl;
+		cin >> correctYear;
+		if (correctYear == "No" || correctYear == "no") {
+			yearOfManufacture = 0;
+			findManufactureYear();
+		}else if (correctYear == "yes" || correctYear == "Yes"){
+			manfactureFilterKey = yearOfManufacture%100;
+			cout << manfactureFilterKey;
+			findCylinders();
+		}else if (make == "quit" || make == "Quit") {
+			exit(0);
+		}else{
+			cout << "Invaild year; please enter a date between 1995 and 2015" << endl;
+			yearOfManufacture = 0;
+			findManufactureYear;
+		}
+}
 
-void quit() {}
+
+
+
