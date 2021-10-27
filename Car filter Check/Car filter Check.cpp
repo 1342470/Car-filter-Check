a9f1f5621adc1eec052e2b6752c19a2a9e8d904b
1342470 <1342470@stu.ucenmanchester.ac.uk>
2021-10-27 16:24:16 +0100

    inital commit with basic stuture of programme which includes the instance varibles needed for the programme as well as the functions without any body.

---
+++ Car filter Check/Car filter Check.cpp
@@ -0,0 +1,24 @@
+// Car filter Check.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+string make;
+int yearOfManufacture;
+int numOfCylinders;
+
+
+int main()
+{
+	cout << "welcome to the Car filter search when prompted please enter the correct respective data" << endl;
+
+
+}
+
+void findMake() {}
+
+void findManufacture() {}
+
+void findCylinders() {}
+
+void quit() {}
